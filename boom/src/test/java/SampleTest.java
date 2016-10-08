a58bcffc5975530a1900dab7574b7a19c3cb42b0
César <cesaryomismo@gmail.com>
2016-10-08 16:53:37 +0100

    Base Structure

---
+++ boom/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by cesards on 08/10/2016.
+ */
+public class SampleTest {
+    @Test
+    public void name() throws Exception {
+        throw new Exception("bla");
+    }
+}
